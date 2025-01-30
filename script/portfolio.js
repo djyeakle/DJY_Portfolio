cc3fd365dc5d48d2a900fe3e121226c9fc3ff7f5
Delaney <djyeakl@pointpark.edu>
2025-01-30 11:42:02 -0500

    Second round of portfolio updates

---
+++ script/portfolio.js
@@ -0,0 +1,11 @@
+function vinnie() {
+    window.open('https://vvcmtwinery.netlify.app/', '_blank');
+}
+
+function thai() {
+    window.open('https://spagettisthaicuisine.netlify.app/', '_blank');
+}
+
+function berry() {
+    window.open('https://watch.screencastify.com/v/SX9ZF9G16i5HuEtSpb5x', '_blank');
+}
