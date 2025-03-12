1f6fdb4ee62f2300b80520e80fbed6b3f61a7a81
Delaney <djyeakl@pointpark.edu>
2025-03-12 17:57:07 -0400

    added courses and resume page

---
+++ script/contact.js
@@ -0,0 +1,7 @@
+function linkedin() {
+    window.open('https://www.linkedin.com/in/delaney-yeakle-98803028a/', '_blank');
+}
+
+function handshake() {
+    window.open('https://app.joinhandshake.com/profiles/pmsjps', '_blank');
+}
